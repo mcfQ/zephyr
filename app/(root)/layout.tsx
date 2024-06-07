6d6ad49992f3b3a218fba4a6dda57c8526a202a6
Maverick <tayqinyuan@gmail.com>
2024-06-07 20:17:27 +0100

    start implementation of mobile navbar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileNav from "@/components/MobileNav"
+import SideBar from "@/components/SideBar"
+import Image from "next/image"
+
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <main>
-      SIDEBAR
-      {children}
-    </main>
-  );
+	const loggedIn = { firstName: "Q", lastName: "T" }
+
+	return (
+		<main className='flex h-screen w-full font-inter'>
+			<SideBar user={loggedIn} />
+			<div className='flex size-full flex-col'>
+				<div className='root-layout'>
+					<Image src='/icons/logo.svg' width={30} height={30} alt='menu icon' />
+					<div>
+						<MobileNav user={loggedIn} />
+					</div>
+				</div>
+				{children}
+			</div>
+		</main>
+	)
 }
